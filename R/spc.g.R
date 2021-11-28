10dc06a2cf0fe6a52ae9cd4cd4662a15e398567b
MMunozMarquez <manuel.munoz.marquez@gmail.com>
2021-11-28 19:07:54 +0100

    Prepare R/spc.g.R for translation

--- R/spc.g.R
+++ R/spc.g.R
@@ -16,14 +16,14 @@
 "limits.g" <- function(center, std.dev, sizes, nsigmas = NULL, conf = NULL)
 {
   if(is.null(nsigmas) & is.null(conf))
-    stop("Argument 'nsigmas' or 'conf' must be provided. See help.")
+    stop(gettext("Argument 'nsigmas' or 'conf' must be provided. See help."))
   if (is.null(conf)) 
     {
       p <- 1/center
       lcl <- center - conf * sqrt(1-p)/p
       lcl[lcl < 0] <- 0
       ucl <- center + conf * sqrt(1-p)/p
-      warning("The Geometric distribution is quite skewed, it is better to set conf at the required confidence level (0 < conf < 1) instead of as a multiplier of sigma.")
+      warning(gettext("The Geometric distribution is quite skewed, it is better to set conf at the required confidence level (0 < conf < 1) instead of as a multiplier of sigma."))
   }
   else {
     if (conf > 0 & conf < 1) {
@@ -31,7 +31,7 @@
       ucl <- qgeom(1 - (1 - conf)/2, p)
       lcl <- qgeom((1 - conf)/2, p)
     }
-    else stop("invalid conf argument. See help.")
+    else stop(gettext("invalid conf argument. See help."))
   }
   limits <- matrix(c(lcl, ucl), ncol = 2)
   rownames(limits) <- rep("", length = nrow(limits))
